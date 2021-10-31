471f3b66e3e4eabd7a665a7f0463e49b977c33fb
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-10-31 17:28:48 +0100

    init

---
+++ src/isItFriday.js
@@ -0,0 +1,10 @@
+module.exports =  isItFriday(today) {
+  if (today === "Friday") {
+    return "TGIF";
+  } else {
+    return "Nope";
+  }
+}
+
+//export { isItFriday };
+//module.exports = "isItFriday"
